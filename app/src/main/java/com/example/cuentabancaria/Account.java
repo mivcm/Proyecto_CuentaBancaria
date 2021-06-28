d45097e0cec5538c7490f2ea3d39bd3416124aec
mivcm <mivcm2001@gmail.com>
2021-06-28 00:15:54 -0500

    Se agrego la funcionalidad de pasar de Activity obteniendo el nombre del usuario, además en la activity nueva se implementaron los métodos para depositar y retirar, aún se continua trabajando en ello.

--- app/src/main/java/com/example/cuentabancaria/Account.java
+++ app/src/main/java/com/example/cuentabancaria/Account.java
@@ -3,12 +3,47 @@ package com.example.cuentabancaria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Account extends AppCompatActivity {
+    private TextView txtUsuario, txtSaldo;
+    private EditText etxtDeposito, etxtRetiro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtSaldo = findViewById(R.id.txtSaldo);
+        etxtDeposito = findViewById(R.id.etxtDeposito);
+        etxtRetiro = findViewById(R.id.etxtRetiro);
+        String enviar = getIntent().getStringExtra("Usuario");
+        txtUsuario.setText(enviar);
+    }
+
+    public void deposito (View view){
+        int saldo = Integer.parseInt(txtSaldo.getText().toString());
+        int depositar = Integer.parseInt(etxtDeposito.getText().toString());
+        int suma = saldo + depositar;
+        String resul = String.valueOf(suma);
+        txtSaldo.setText(resul);
+    }
+
+    public void retiro (View v){
+        int saldo = Integer.parseInt(txtSaldo.getText().toString());
+        int retirar = Integer.parseInt(etxtRetiro.getText().toString());
+        int resta = saldo - retirar;
+        String resul =  String.valueOf(resta);
+        txtSaldo.setText(resul);
+    }
+
+    public void confirmar (View vi){
+        if (etxtRetiro != null){
+            retiro(null);
+        } else if (etxtDeposito != null){
+            deposito(null);
+        }
     }
 }
--- app/src/main/java/com/example/cuentabancaria/MainActivity.java
+++ app/src/main/java/com/example/cuentabancaria/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.cuentabancaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etxtUsuario, etxtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etxtUsuario = findViewById(R.id.etxtUsuario);
+        etxtPassword = findViewById(R.id.etxtPassword);
+        SharedPreferences p = getSharedPreferences("Datos", Context.MODE_PRIVATE);
+        etxtUsuario.setText(p.getString("Usuario", " "));
+        etxtPassword.setText(p.getString("Password", " "));
+    }
+
+    public void crearCuenta (View view){
+        SharedPreferences p = getSharedPreferences("Datos", Context.MODE_PRIVATE);
+        String usuario = etxtUsuario.getText().toString();
+        String password = etxtPassword.getText().toString();
+        SharedPreferences.Editor e = p.edit();
+        e.putString("Usuario", etxtUsuario.getText().toString());
+        e.putString("Password", etxtPassword.getText().toString());
+        etxtUsuario.setText(usuario);
+        etxtPassword.setText(password);
+        e.commit();
+        finish();
+    }
+
+    public void  ingresar (View view){
+        Intent enviar = new Intent(this, Account.class);
+        enviar.putExtra("Usuario", etxtUsuario.getText().toString());
+        startActivity(enviar);
     }
 }
